d5fef9a770580302f27980ea3558f8f9f1befdaa
Daniel <A01378688@itesm.mx>
2021-03-29 22:29:34 -0600

    Música y animación hoyo negro en nivel 1-2

--- Assets/Scripts/RocketCrash.cs
+++ Assets/Scripts/RocketCrash.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RocketCrash : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision) 
     {
         if (collision.gameObject.CompareTag("Enemy")){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+
+        if (collision.gameObject.CompareTag("BlackHole")){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
