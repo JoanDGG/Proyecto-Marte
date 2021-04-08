63f08e59407199c98f0219892112f8fc35d31bbb
luisferro2 <a01378248@itesm.mx>
2021-04-07 19:01:28 -0500

    Intento de arreglar longitud de chasis.
    Intento arreglar la longitud del chasis cuando se cambia el cuerpo del auto, casi listo.

--- Assets/Scripts/scriptsIndustrial/funcionesBotonesSprites.cs
+++ Assets/Scripts/scriptsIndustrial/funcionesBotonesSprites.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 /*
  Script que hace que cambien los sprites del automovil cuando el usuario haga click entre las opciones
 Autor: Luis Ignacio Ferro Salinas A01378248
-Última actualización: 5 de abril 
+Última actualización: 7 de abril 
  */
 
 public class funcionesBotonesSprites : MonoBehaviour
@@ -16,13 +16,16 @@ public class funcionesBotonesSprites : MonoBehaviour
     // El GameObject que contiene al automóvil que se muestra en pantalla.
     public GameObject automovil;
 
+    // El chasis del automóvil necesario para saber cuál poner si cambia el cuerpo.
+    public int spriteChasis = 12;
+
     public void CambiaParte(int indiceParte, int indiceSprite) {
         // Función para cambiar todas las partes del automóvil, teniendo una opción de sprite seleccionada.
 
         // Para partes que solo se encuentran 1 vez en el auto (cuerpo, motor, chasis)
         if (indiceParte == 0 || (indiceParte >= 4 && indiceParte <= 5))
         {
-            // Movemos las partes necesarias si el cuerpo se alarga
+            // Movemos las partes necesarias si el cuerpo se alarga.
             if (indiceParte == 0 && indiceSprite != 0) {
                 // Llantas.
                 automovil.transform.GetChild(1).GetChild(0).localPosition = new Vector3(2.4f, -1.6f, 0);
@@ -38,6 +41,11 @@ public class funcionesBotonesSprites : MonoBehaviour
 
                 // Motor.
                 automovil.transform.GetChild(5).localPosition = new Vector3(3.2f, -0.8f, 0);
+
+                // Chasis.
+                automovil.transform.GetChild(4).gameObject.GetComponent<SpriteRenderer>().sprite = this.transform.GetChild(1).GetChild(spriteChasis).gameObject.GetComponent<Image>().sprite;
+
+                
             }
             else if (indiceParte == 0 && indiceSprite == 0){
                 // Llantas.
@@ -54,6 +62,10 @@ public class funcionesBotonesSprites : MonoBehaviour
 
                 // Motor.
                 automovil.transform.GetChild(5).localPosition = new Vector3(1.6f, -0.8f, 0);
+
+                // Chasis.
+                automovil.transform.GetChild(4).gameObject.GetComponent<SpriteRenderer>().sprite = this.transform.GetChild(1).GetChild(spriteChasis).gameObject.GetComponent<Image>().sprite;
+
             }
 
             // Cambio de sprite.
@@ -122,46 +134,55 @@ public class funcionesBotonesSprites : MonoBehaviour
 
     public void CambiaChasis1() {
         CambiaParte(4, 0);
+        spriteChasis = 12;
     }
 
     public void CambiaChasis2()
     {
         CambiaParte(4, 1);
+        spriteChasis = 13;
     }
 
     public void CambiaChasis3()
     {
         CambiaParte(4, 2);
+        spriteChasis = 14;
     }
 
     public void CambiaChasis4()
     {
         CambiaParte(4, 3);
+        spriteChasis = 15;
     }
 
     public void CambiaChasis5()
     {
         CambiaParte(4, 4);
+        spriteChasis = 16;
     }
 
     public void CambiaChasis6()
     {
         CambiaParte(4, 5);
+        spriteChasis = 17;
     }
 
     public void CambiaChasis7()
     {
         CambiaParte(4, 6);
+        spriteChasis = 18;
     }
 
     public void CambiaChasis8()
     {
         CambiaParte(4, 7);
+        spriteChasis = 19;
     }
 
     public void CambiaChasis9()
     {
         CambiaParte(4, 8);
+        spriteChasis = 20;
     }
 
     public void CambiaMotor1() {
