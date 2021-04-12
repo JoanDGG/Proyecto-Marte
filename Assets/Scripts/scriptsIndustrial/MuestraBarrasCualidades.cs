abf4a1d5ed687aa6c5781c332aecb0e0b3a9555b
luisferro2 <a01378248@itesm.mx>
2021-04-12 13:28:21 -0500

    Pointer de prueba botón de cuerpo
    Agrego las funciones para que se muestre el panel describiendo a la parte del cuerpo cuando el usuario pasa el mouse por el botón del cuerpo.

--- Assets/Scripts/scriptsIndustrial/MuestraBarrasCualidades.cs
+++ Assets/Scripts/scriptsIndustrial/MuestraBarrasCualidades.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 /*
  Este script contiene las funciones que permiten desplegar un panel con la información de cada parte del automóvil.
  Autor: Luis Ignacio Ferro Salinas A01378248
- Última actualización: 8 de abril de 2021.
+ Última actualización: 12 de abril de 2021.
  */
 
 public class MuestraBarrasCualidades : MonoBehaviour
 {
+    public void MuestraBarraCuerpo1() {
+        this.transform.GetChild(2).GetChild(0).gameObject.SetActive(true);
+    }
+
+    public void ApagaBarraCuerpo1() {
+        this.transform.GetChild(2).GetChild(0).gameObject.SetActive(false);
+
+    }
+
+    private void ApagaPanelesEstadisticas() {
+        // Apago todos los paneles de estadisticas al iniciar la escena.
+        this.transform.GetChild(2).GetChild(0).gameObject.SetActive(false);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ApagaPanelesEstadisticas();
     }
 
     // Update is called once per frame
