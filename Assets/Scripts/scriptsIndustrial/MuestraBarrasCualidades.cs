a46ae66165d302e164523cb8b6e1d7f849031118
luisferro2 <a01378248@itesm.mx>
2021-04-12 19:47:17 -0500

    paneles solares
    Ya aparecen los páneles al hacer mouse over en todos los botones pertinentes del menú, sistema de presupuestos está diseñado y falta implementarlo.

--- Assets/Scripts/scriptsIndustrial/MuestraBarrasCualidades.cs
+++ Assets/Scripts/scriptsIndustrial/MuestraBarrasCualidades.cs
@@ -18,38 +18,223 @@ public class MuestraBarrasCualidades : MonoBehaviour
         this.transform.GetChild(parte).gameObject.SetActive(false);
     }
 
+
     public void MuestraBarraCuerpo1() {
-        MuestraBarraParte(3);
-    }
-
-    public void ApagaBarraCuerpo1() {
-        ApagaBarraParte(3);
-    }
-
-    public void MuestraBarraCuerpo2()
-    {
         MuestraBarraParte(4);
     }
 
-    public void ApagaBarraCuerpo2()
-    {
+    public void ApagaBarraCuerpo1() {
         ApagaBarraParte(4);
     }
-    public void MuestraBarraCuerpo3()
+
+
+    public void MuestraBarraCuerpo2()
     {
         MuestraBarraParte(5);
     }
 
-    public void ApagaBarraCuerpo3()
+    public void ApagaBarraCuerpo2()
     {
         ApagaBarraParte(5);
     }
 
+
+    public void MuestraBarraCuerpo3()
+    {
+        MuestraBarraParte(6);
+    }
+
+    public void ApagaBarraCuerpo3()
+    {
+        ApagaBarraParte(6);
+    }
+
+
+    public void MuestraBarraLlantas1()
+    {
+        MuestraBarraParte(7);
+    }
+
+    public void ApagaBarraLlantas1()
+    {
+        ApagaBarraParte(7);
+    }
+
+
+    public void MuestraBarraLlantas2()
+    {
+        MuestraBarraParte(8);
+    }
+
+    public void ApagaBarraLlantas2()
+    {
+        ApagaBarraParte(8);
+    }
+
+
+    public void MuestraBarraLlantas3()
+    {
+        MuestraBarraParte(9);
+    }
+
+    public void ApagaBarraLlantas3()
+    {
+        ApagaBarraParte(9);
+    }
+
+
+    public void MuestraBarraFrenos1()
+    {
+        MuestraBarraParte(10);
+    }
+
+    public void ApagaBarraFrenos1()
+    {
+        ApagaBarraParte(10);
+    }
+
+
+    public void MuestraBarraFrenos2()
+    {
+        MuestraBarraParte(11);
+    }
+
+    public void ApagaBarraFrenos2()
+    {
+        ApagaBarraParte(11);
+    }
+
+
+    public void MuestraBarraFrenos3()
+    {
+        MuestraBarraParte(12);
+    }
+
+    public void ApagaBarraFrenos3()
+    {
+        ApagaBarraParte(12);
+    }
+
+
+    public void MuestraBarraSuspension1()
+    {
+        MuestraBarraParte(13);
+    }
+
+    public void ApagaBarraSuspension1()
+    {
+        ApagaBarraParte(13);
+    }
+
+
+    public void MuestraBarraSuspension2()
+    {
+        MuestraBarraParte(14);
+    }
+
+    public void ApagaBarraSuspension2()
+    {
+        ApagaBarraParte(14);
+    }
+
+
+    public void MuestraBarraSuspension3()
+    {
+        MuestraBarraParte(15);
+    }
+
+    public void ApagaBarraSuspension3()
+    {
+        ApagaBarraParte(15);
+    }
+
+
+    public void MuestraBarraChasis1()
+    {
+        MuestraBarraParte(16);
+    }
+
+    public void ApagaBarraChasis1()
+    {
+        ApagaBarraParte(16);
+    }
+
+
+    public void MuestraBarraChasis2()
+    {
+        MuestraBarraParte(17);
+    }
+
+    public void ApagaBarraChasis2()
+    {
+        ApagaBarraParte(17);
+    }
+
+
+    public void MuestraBarraChasis3()
+    {
+        MuestraBarraParte(18);
+    }
+
+    public void ApagaBarraChasis3()
+    {
+        ApagaBarraParte(18);
+    }
+
+
+    public void MuestraBarraMotor1()
+    {
+        MuestraBarraParte(19);
+    }
+
+    public void ApagaBarraMotor1()
+    {
+        ApagaBarraParte(19);
+    }
+
+
+    public void MuestraBarraMotor2()
+    {
+        MuestraBarraParte(20);
+    }
+
+    public void ApagaBarraMotor2()
+    {
+        ApagaBarraParte(20);
+    }
+
+
+    public void MuestraBarraMotor3()
+    {
+        MuestraBarraParte(21);
+    }
+
+    public void ApagaBarraMotor3()
+    {
+        ApagaBarraParte(21);
+    }
+
+
     private void ApagaPanelesEstadisticas() {
         // Apago todos los paneles de estadisticas al iniciar la escena.
-        ApagaBarraParte(3);
         ApagaBarraParte(4);
         ApagaBarraParte(5);
+        ApagaBarraParte(6);
+        ApagaBarraParte(7);
+        ApagaBarraParte(8);
+        ApagaBarraParte(9);
+        ApagaBarraParte(10);
+        ApagaBarraParte(11);
+        ApagaBarraParte(12);
+        ApagaBarraParte(13);
+        ApagaBarraParte(14);
+        ApagaBarraParte(15);
+        ApagaBarraParte(16);
+        ApagaBarraParte(17);
+        ApagaBarraParte(18);
+        ApagaBarraParte(19);
+        ApagaBarraParte(20);
+        ApagaBarraParte(21);
     }
 
     // Start is called before the first frame update
