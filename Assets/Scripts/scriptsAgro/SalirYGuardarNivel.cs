da7e7db08b0c9794896593b178f98511ede3abce
A01378650 <65170426+A01378650@users.noreply.github.com>
2021-04-21 10:16:01 -0500

    Correccion errores de compilacion

--- Assets/Scripts/scriptsAgro/SalirYGuardarNivel.cs
+++ Assets/Scripts/scriptsAgro/SalirYGuardarNivel.cs
@@ -1,22 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SalirYGuardarNivel : MonoBehaviour
 {
     public void SalirMenu()
     {
-        PlayerPrefs.SetInt("primero", GameManager.primero);
+        PlayerPrefs.SetInt("primero", (GameManager.primero) ? 1:0);
         PlayerPrefs.SetInt("tiempo", GameManager.tiempo);
         PlayerPrefs.SetInt("tiempoLimite", GameManager.tiempoLimite);
-        PlayerPrefs.SetInt("evento", GameManager.evento);
+        PlayerPrefs.SetInt("evento", (GameManager.evento) ? 1:0);
         PlayerPrefs.SetInt("tiempoEvento", GameManager.tiempoEvento);
         PlayerPrefs.SetInt("reloj", GameManager.reloj);
-        PlayerPrefs.SetInt("resist0", GameManager.resist[0]);
-        PlayerPrefs.SetInt("resist1", GameManager.resist[1]);
-        PlayerPrefs.SetInt("resist2", GameManager.resist[2]);
-        PlayerPrefs.SetInt("resist3", GameManager.resist[3]);
-        PlayerPrefs.SetInt("perder", GameManager.perder);
+        PlayerPrefs.SetInt("resist0", (GameManager.resist[0]) ? 1:0);
+        PlayerPrefs.SetInt("resist1", (GameManager.resist[1]) ? 1:0);
+        PlayerPrefs.SetInt("resist2", (GameManager.resist[2]) ? 1:0);
+        PlayerPrefs.SetInt("resist3", (GameManager.resist[3]) ? 1:0);
         PlayerPrefs.SetInt("pagina", GameManager.pagina);
         PlayerPrefs.SetInt("clima0", GameManager.clima[0]);
         PlayerPrefs.SetInt("clima1", GameManager.clima[1]);
@@ -25,11 +25,11 @@ public class SalirYGuardarNivel : MonoBehaviour
         PlayerPrefs.SetInt("genes1", GameManager.genes[1]);
         PlayerPrefs.SetInt("genes2", GameManager.genes[2]);
         PlayerPrefs.SetInt("oleada", GameManager.oleada);
-        PlayerPrefs.SetInt("respondido", GameManager.oleada);
-        PlayerPrefs.SetInt("respuestas0", GameManager.respuestas[0]);
-        PlayerPrefs.SetInt("respuestas1", GameManager.respuestas[1]);
-        PlayerPrefs.SetInt("respuestas2", GameManager.respuestas[2]);
-        PlayerPrefs.SetInt("puntuacion", GameManager.puntuacion);
+        PlayerPrefs.SetInt("respondido", (GameManager.respondido) ? 1:0);
+        PlayerPrefs.SetString("respuestas0", GameManager.respuestas[0]);
+        PlayerPrefs.SetString("respuestas1", GameManager.respuestas[1]);
+        PlayerPrefs.SetString("respuestas2", GameManager.respuestas[2]);
+        PlayerPrefs.SetFloat("puntuacion", GameManager.puntuacion);
         PlayerPrefs.Save();
         SceneManager.LoadScene("MenuMisiones");
     }
