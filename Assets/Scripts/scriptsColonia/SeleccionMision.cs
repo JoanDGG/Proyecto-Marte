45fa9ff0fb6c0a6ea459e0a32c4a10e74597ed84
Daniel <A01378688@itesm.mx>
2021-04-23 00:17:02 -0500

    Agregando canvas a colonia

--- Assets/Scripts/scriptsColonia/SeleccionMision.cs
+++ Assets/Scripts/scriptsColonia/SeleccionMision.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class SeleccionMision : MonoBehaviour
 {
+    public Canvas canvasMisionCohete;
+    public Canvas canvasMisionRobot;
+
+    void Start()
+    {
+        canvasMisionCohete.enabled = false;
+        canvasMisionRobot.enabled = false;
+    }
     private void OnTriggerEnter2D(Collider2D collider) 
     {
         if (collider.gameObject.CompareTag("SeleccionCohete")){
-            //gameObject.GetComponent<SpriteRenderer>().enabled = false;
             print("¿Quieres entrar a esta mision?");
+            canvasMisionCohete.enabled = true;
         }
 
+        if (collider.gameObject.CompareTag("SeleccionMaquinas")){
+            print("¿Quieres entrar a esta mision?");
+            canvasMisionRobot.enabled = true;
+        }
+    }
 
+    private void OnTriggerExit2D() 
+    {
+        canvasMisionCohete.enabled = false;
+        canvasMisionRobot.enabled = false;
     }
 }
