8f9125633dd975f2605fd5c4c251c6eb14dce61e
A01378650 <65170426+A01378650@users.noreply.github.com>
2021-04-23 13:40:53 -0500

    Log in

---
+++ Assets/Scripts/Red.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Red : MonoBehaviour
+{
+
+    public Text Gamertag;
+    public InputField Contraseña;
+    public Text resultado;
+    public static DateTime inicio;
+    public static string textoPlano;
+    
+    public void EscribirTextoPlano()
+    {
+        StartCoroutine(SubirTextoPlano());
+    }
+
+    private IEnumerator SubirTextoPlano()
+    {
+        WWWForm forma = new WWWForm();
+        forma.AddField("gamertag", Gamertag.text);
+        forma.AddField("contrasena", Contraseña.text);
+        UnityWebRequest request = UnityWebRequest.Post("http://localhost:8080/jugador/LogIn", forma);
+        yield return request.SendWebRequest();
+        bool exito = true;
+        print(request.result);
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            textoPlano = request.downloadHandler.text;
+            string texto = "Log in exitoso, bienvenido";
+            if (textoPlano != Gamertag.text)
+            {
+                texto = "No se encontró el usuario";
+                exito = false;
+            }
+            else
+            {
+
+            }
+            resultado.text = texto;
+        }
+        else
+        {
+            resultado.text = "Error en la descarga: " + request.responseCode.ToString();
+        }
+        if (exito)
+        {
+            yield return new WaitForSeconds(3.0f);
+            inicio = System.DateTime.Now;
+            SceneManager.LoadScene("ColoniaMarte");
+        }
+    }
+}
