50baadcff6e44cff0e59e5b7761346ac94db05af
A01378650 <65170426+A01378650@users.noreply.github.com>
2021-04-27 20:07:23 -0500

    Cambio movimiento

---
+++ Assets/Scripts/MovimientoGeneral.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoGeneral : MonoBehaviour
+{
+
+    public float velocidad = 7;
+    private Rigidbody2D rigidbody;
+    private SpriteRenderer sprRenderer;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movHorizontal = Input.GetAxis("Horizontal");
+        float movVertical = Input.GetAxis("Vertical");
+        rigidbody.velocity = new Vector2(movHorizontal * velocidad, movVertical * velocidad);
+        float ABSvelocidad = Mathf.Abs((rigidbody.velocity.x + rigidbody.velocity.y));
+        anim.SetFloat("velocidad", ABSvelocidad);
+        if (rigidbody.velocity.x > 0.1)
+        {
+            sprRenderer.flipX = false;
+        }
+        else if (rigidbody.velocity.x < -0.1)
+        {
+            sprRenderer.flipX = true;
+        }
+    }
+}
